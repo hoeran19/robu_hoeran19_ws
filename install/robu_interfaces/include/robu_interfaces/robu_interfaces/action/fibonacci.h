3732d79541b43f7d89323722aaa3d407e2fe68d6
hoeran19 <hoeran19@htl-kaindorf.at>
2024-02-13 13:04:56 +0100

    action

---
+++ install/robu_interfaces/include/robu_interfaces/robu_interfaces/action/fibonacci.h
@@ -0,0 +1,12 @@
+// generated from rosidl_generator_c/resource/idl.h.em
+// with input from robu_interfaces:action/Fibonacci.idl
+// generated code does not contain a copyright notice
+
+#ifndef ROBU_INTERFACES__ACTION__FIBONACCI_H_
+#define ROBU_INTERFACES__ACTION__FIBONACCI_H_
+
+#include "robu_interfaces/action/detail/fibonacci__struct.h"
+#include "robu_interfaces/action/detail/fibonacci__functions.h"
+#include "robu_interfaces/action/detail/fibonacci__type_support.h"
+
+#endif  // ROBU_INTERFACES__ACTION__FIBONACCI_H_
---
+++ install/robu_interfaces/include/robu_interfaces/robu_interfaces/action/fibonacci.hpp
@@ -0,0 +1,11 @@
+// generated from rosidl_generator_cpp/resource/idl.hpp.em
+// generated code does not contain a copyright notice
+
+#ifndef ROBU_INTERFACES__ACTION__FIBONACCI_HPP_
+#define ROBU_INTERFACES__ACTION__FIBONACCI_HPP_
+
+#include "robu_interfaces/action/detail/fibonacci__struct.hpp"
+#include "robu_interfaces/action/detail/fibonacci__builder.hpp"
+#include "robu_interfaces/action/detail/fibonacci__traits.hpp"
+
+#endif  // ROBU_INTERFACES__ACTION__FIBONACCI_HPP_
