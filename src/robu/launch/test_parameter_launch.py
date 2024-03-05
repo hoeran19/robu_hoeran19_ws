e30107f34701848a40f6bdb0ff1e1751d0fba414
hoeran19 <hoeran19@htl-kaindorf.at>
2024-03-05 14:49:30 +0100

    jkl

--- src/robu/launch/test_parameter_launch.py
+++ src/robu/launch/test_parameter_launch.py
@@ -1,6 +1,7 @@
 #import os
 import launch
 import launch_ros.actions
+
 #from launch.actions.execute_process import ExecuteProcess
 #from ament_index_python.packages import get_package_share_directory
 #from launch.substitutions import LaunchConfiguration
@@ -14,6 +15,7 @@ import launch_ros.actions
 def generate_launch_description():
 
     return launch.LaunchDescription([
+
 
         launch_ros.actions.Node(
             package='robu',
