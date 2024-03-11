728b18b8787ff7f5613768ce869ed151896de7fa
hoeran19 <hoeran19@htl-kaindorf.at>
2024-03-11 11:00:38 +0100

    hkj

--- install/robu/share/robu/ex13_action_launch.py
+++ install/robu/share/robu/ex13_action_launch.py
@@ -1,36 +1,33 @@
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, SetEnvironmentVariable
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
 from launch_ros.actions import Node
 
 def generate_launch_description():
    
-    #fibonacci_server = os.path.join(
-    #    os.getcwd(), '/home/robu/work/robu_hoeran19_ws/src/robu/robu/ex12_fibonacci_server.py')
-    #fibonacci_client = os.path.join(
-    #    os.getcwd(), '/home/robu/work/robu_hoeran19_ws/src/robu/robu/ex12_fibonacci_client.py')
-
-    # Erstelle eine Liste von Actions für die Launch-Datei
+    fibonacci_server = os.path.join(
+        os.getcwd(), '/home/robu/work/robu_hoeran19_ws/src/robu/robu/ex12_fibonacci_server.py')
+    fibonacci_client = os.path.join(
+        os.getcwd(), '/home/robu/work/robu_hoeran19_ws/src/robu/robu/ex12_fibonacci_client.py')
     return LaunchDescription([
     
-#        Node(
-#           package='robu',
-#            executable='fibonacci_server',
-#            output='screen',
-#            name='fibonacci_server'
-#        ),
-#        # Starte den Fibonacci Action-Client als ROS 2 Node
-#        Node(
-#            package='robu',
-#            executable='fibonacci_client',
-#            output='screen',
-#            name='fibonacci_client'
-#        ),
+        SetEnvironmentVariable('ROS_DOMAIN_ID', '7'),
+
+        Node(
+           package='robu',
+            executable='fibonacci_server',
+            output='screen',
+            name='fibonacci_server'
+        ),
         
-        # Warte 5 Sekunden, bevor die CLI-Anweisung ausgeführt wird
-        SetEnvironmentVariable(name='ROS_DOMAIN_ID', value='7'),
+        Node(
+            package='robu',
+            executable='fibonacci_client',
+            output='screen',
+            name='fibonacci_client'
+        ),
+        
+        
 
         ExecuteProcess(
             cmd=['sleep', '5'],
